c1521bd1ad6f8a279ccbebe48e0872d7da00d539
FunriLy <fr479910770@qq.com>
2017-07-10 09:53:17 +0800

    project init

---
+++ src/main/java/com/qg/AnyWork/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.qg.AnyWork.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by FunriLy on 2017/7/10.
+ * From small beginnings comes great things.
+ */
+@Mapper
+@Repository
+public interface UserDao {
+}
---
+++ src/main/java/com/qg/AnyWork/model/User.java
@@ -0,0 +1,8 @@
+package com.qg.AnyWork.model;
+
+/**
+ * Created by FunriLy on 2017/7/10.
+ * From small beginnings comes great things.
+ */
+public class User {
+}

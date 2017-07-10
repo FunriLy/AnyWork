aff25fae4d117dba6289405e656ca6fccc81abe0
FunriLy <fr479910770@qq.com>
2017-07-10 09:21:31 +0800

    project init

---
+++ src/main/java/com/qg/AnyWork/AnyWorkApplication.java
@@ -0,0 +1,12 @@
+package com.qg.AnyWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyWorkApplication.class, args);
+	}
+}

4ca27d884a7aaa53cab600ec406e5e0232dbdffc
logan <153212028@qq.com>
2017-07-14 08:39:56 +0800

    test change

--- src/test/java/com/qg/AnyWork/test.java
+++ src/test/java/com/qg/AnyWork/test.java
@@ -8,9 +8,38 @@ import java.io.*;
  * Created by logan on 2017/7/10.
  */
 public class test {
-    public static void main(String[] args){
-        String SPLIT = "#";
-        String[] answer = "3#8".split("#");
-        System.out.println(answer.length);
+    public static void main(String [] args){
+        System.out.println(new B().getValue());
+    }
+    static class A{
+        protected int value;
+        public A(int v) {
+            setValue(v);
+        }
+        public void setValue(int value){
+            this.value = value;
+        }
+        public int getValue(){
+            try{
+                value++;
+                return value;
+            } catch(Exception e){
+                System.out.println(e.toString());
+            } finally {
+                this.setValue(value);
+                System.out.println(value);
+//                return value;
+            }
+            return value;
+        }
+    }
+    static class B extends A{
+        public B() {
+            super(5);
+            setValue(getValue() - 3);
+        }
+        public void setValue(int value){
+            super.setValue(2 * value);
+        }
     }
 }
